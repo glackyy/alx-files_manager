941d99876b0face4142821ed85a3814596e0aa22
glackyy <othmanesticky@gmail.com>
2024-03-11 14:14:08 +0000

    UserController

---
+++ controllers/UsersController.js
@@ -0,0 +1,7 @@
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import Queue from 'bull';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const userQ = new Queue('userQueue', 'redis://127.0.0.1:6379');
