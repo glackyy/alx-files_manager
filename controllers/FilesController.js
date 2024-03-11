fdb679103bb2e37456c881c55d534d5f2d775069
glackyy <othmanesticky@gmail.com>
2024-03-11 19:12:38 +0000

    FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -202,5 +202,25 @@ class FilesController {
       });
       return null;
     }
-}    
+
+    static async putUnpublish(request, response) {
+      const user = await FilesController.getUser(request);
+      if (!user) {
+        return response.status(401).json({ error: 'Unauthorized' });
+      }
+      const { id } = request.params;
+      const files = dbClient.client.db().collection('files');
+      const idObject = new ObjectID(id);
+      const newValue = { $set: { isPublic: false } };
+      const options = { returnOriginal: false };
+      files.findOneAndUpdate({ _id: idObject, userId: user._id }, newValue, options, (error, file) => {
+        if (!file.lastErrorObject.updateExisting) {
+          return response.status(404).json({ error: 'Not found' });
+        }
+        return response.status(200).json(file.value);
+      });
+      return null;
+    }
+
+  }    
 module.exports = FilesController;
