cf558506e784f84aaf85ebbccf5e14327fa93d9f
glackyy <othmanesticky@gmail.com>
2024-03-11 10:03:52 +0000

    utils/db.js

--- utils/db.js
+++ utils/db.js
@@ -4,3 +4,14 @@ const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
