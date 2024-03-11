e78a7d590e326a2497f12526a3582b3c4a8d84a9
glackyy <othmanesticky@gmail.com>
2024-03-11 17:56:44 +0000

    FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,3 +5,13 @@ import Queue from 'bull';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 import mime from 'mime-types';
+
+const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
+
+class FilesController {
+  static async getUser(request) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+  }
+}
