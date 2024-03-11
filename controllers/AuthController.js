9d37fd953a19733efac1fc1ad0ef109f2878e0da
glackyy <othmanesticky@gmail.com>
2024-03-11 15:19:17 +0000

    AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,3 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+class AuthController {
+  static async getConnect(request, response) {
+    const authData = request.header('Authorization');
+    let userEmail = authData.split(' ')[1];
+    const buff = Buffer.from(userEmail, 'base64');
+    userEmail = buff.toString('ascii');
+    const data = userEmail.split(':');
+    if (data.length !== 2) {
+      response.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+  }
+}
